e01a55df2addebe5c1f9559394b8f78b763742fd
rysiuwroc <rysiuwroc@gmail.com>
2021-09-18 04:25:20 +0200

    added renaming discord user option, albion player db record creation setup

---
+++ libs/functions/db/albion-player-create.js
@@ -0,0 +1,17 @@
+const db = require("../../../models/index")
+
+module.exports = {
+    async execute(player) {
+        var result = await db["albion_players"].findAndCountAll({ where: { id: player.id } });
+        if (!result.count) {
+            await db["albion_players"].create({
+                id: player.id,
+                name: player.name,
+                guildId:player.guildId,
+                allianceId:player.allianceId,
+                isActive:player.isActive,
+                flastChecked:player.lastChecked
+            })
+        }
+    },
+}
